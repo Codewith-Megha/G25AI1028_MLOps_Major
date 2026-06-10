05c2ca1b278ba9798a824748233ef6fc0ce8c4f9
Codewith-Megha <pandeytitli21@gmail.com>
2026-06-10 23:58:17 +0530

    Added Flask application

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+
+model = joblib.load("savedmodel.pth")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    prediction = None
+
+    if request.method == "POST":
+
+        file = request.files["image"]
+
+        img = Image.open(file)
+
+        img = img.convert("L")
+
+        img = img.resize((64, 64))
+
+        img_array = np.array(img)
+
+        img_array = img_array.flatten().reshape(1, -1)
+
+        prediction = model.predict(img_array)[0]
+
+    return render_template(
+        "index.html",
+        prediction=prediction
+    )
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
